a5b9697492d6b766434dbf6ed89b60e4058ec254
Jon Pro <jonpro03@gmail.com>
2023-12-28 10:12:08 -0600

    bump version 0.1.1 -> 0.2.0

--- gt_telem/__init__.py
+++ gt_telem/__init__.py
@@ -3,4 +3,4 @@ from gt_telem.turismo_game.events.turismo_driver import TurismoDriver
 from gt_telem.turismo_game.events.turismo_game import TurismoGame
 from gt_telem.turismo_game.events.turismo_race import TurismoRace
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
