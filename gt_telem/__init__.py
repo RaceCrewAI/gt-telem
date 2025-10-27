e12a5ac59f520331574bf729301f45620f5733a5
Anton Ptashnik <iavtomator@gmail.com>
2025-10-27 19:50:34 +0200

    bump version 1.1.1 -> 1.2.0

--- gt_telem/__init__.py
+++ gt_telem/__init__.py
@@ -4,4 +4,4 @@ from gt_telem.events.race_events import RaceEvents
 from gt_telem.turismo_client import TurismoClient
 from gt_telem.trackdetector import TRACK_NAMES
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
